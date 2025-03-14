7752576df01bb6648bc138217455d6f09f04101a
Sama Alaa Hassan <s2590645@gmail.com>
2025-03-15 00:17:57 +0200

    CraftingList

--- CraftingServiceApp.Web/Controllers/HomeController.cs
+++ CraftingServiceApp.Web/Controllers/HomeController.cs
@@ -1,16 +1,23 @@
+﻿using CraftingServiceApp.Infrastructure.Data;
 using CraftingServiceApp.Web.Models;
+using CraftingServiceApp.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore; // مهم لو بتستخدم Include
 using System.Diagnostics;
+
 
 namespace CraftingServiceApp.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context; // أضفنا الـ DbContext هنا
 
-        public HomeController(ILogger<HomeController> logger)
+        // Inject both ILogger و DbContext
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -21,6 +28,23 @@ namespace CraftingServiceApp.Web.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public IActionResult Crafters()
+        {
+            // استخدام _context بدل context
+            var crafters = _context.Users
+                .Include(x => x.Role) // لو مش Include هيعمل NullReference
+                .Where(x => x.Role.Name == "Crafter")
+                .Select(u => new CraftersViewModel
+                {
+                    FullName = u.FullName,
+                    PhoneNumber = u.PhoneNumber,
+                    ProfilePic = u.ProfilePic // Use stored profile picture path
+                })
+               .ToList();
+
+            return View(crafters);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ CraftingServiceApp.Web/ViewModels/CraftersViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CraftingServiceApp.Web.ViewModels
+{
+    public class CraftersViewModel
+    {
+        [Required]
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+
+        [Required]
+        [Phone]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "Profile Picture")]
+        public string ProfilePic { get; set; }
+
+    }
+
+}
